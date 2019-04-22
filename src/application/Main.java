d3bdb0863b2af54292051f8d2cbaf1318c3d1c07
dennvm09 <valenmosquera1999@gmail.com>
2019-04-21 22:41:21 -0500

    application view was modified

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,17 +2,33 @@ package application;
 	
 import javafx.application.Application;
 import javafx.stage.Stage;
+import model.StockExchange;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.fxml.FXMLLoader;
 
 
 public class Main extends Application {
+	
+	private static StockExchange stockExchange;
+	
+	public Main() {
+		stockExchange = new StockExchange("Stock Exchange");
+	}
+	
+	public static StockExchange getStockExchange() {
+		return stockExchange;
+	}
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Pane root = (Pane)FXMLLoader.load(getClass().getResource("Main.fxml"));
+			TabPane root = (TabPane)FXMLLoader.load(getClass().getResource("Main.fxml"));
+			root.setBackground(new Background(new BackgroundFill(Color.WHITE, null, null)));
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
