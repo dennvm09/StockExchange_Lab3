efbb7bea18c03f0124735e3011367fc6f4a06cc5
Lize756 <lize756cas@gmail.com>
2019-04-25 20:42:33 -0500

    final modification of the red and black tree

---
+++ src/redBlack/RedBlackTree.java
@@ -0,0 +1,106 @@
+package redBlack;
+
+import javafx.scene.transform.Rotate;
+import model.CapitalAction;
+
+public class RedBlackTree<T extends Comparable<T>>{
+
+	 private Node root;
+
+	    public Node lookup(T key) {
+	        Node current = root;
+	        while (current.getKey() != key) {
+	            if (key.compareTo((T) current.getKey()) < 0)
+	                current = current.getLeftChild();
+	            else
+	                current = current.getRightChild();
+	            if (current == null)
+	                return null;
+	        }
+	        return current;
+	    }
+	    public void insert(T key) {
+	        if (root == null) {
+	            root = new Node(key, null);
+	            fixTree(root);
+	        } else {
+	            fixTree(root.add(key));
+	        }
+	    }
+	    public void leftRotate(Node x) {
+	        Node y = x.getRightChild();
+	        Node B = y.getLeftChild();
+	        y.setParent(x.getParent());
+	        x.setRightChild(B);
+	        y.setLeftChild(x);
+	        if (x == root) {
+	            root = y;
+	        }
+	    }
+	    public void rightRotate(Node y) {
+	        Node x = y.getLeftChild();
+	        Node B = x.getRightChild();
+	        x.setParent(y.getParent());
+	        y.setLeftChild(B);
+	        x.setRightChild(y);
+	        if (y == root) {
+	            root = x;
+	        }
+	    }
+	    public void fixTree(Node current) {
+	        if (current == root) {
+	            current.setNodeColor(Node.NodeColor.BLACK);
+	            return;
+	        }
+	        if (current.getParent().isBlack()) {
+	            return;
+	        }
+	        Node parent = current.getParent();
+
+	        if (current.isRed() && parent.isRed()) {
+	            Node grandParent = current.getGrandParent();
+	            Node aunt = current.getAunt();
+	            if (aunt == null || aunt.isBlack()) {
+
+	                if (current.isRightChild() && parent.isLeftChild()) {
+	                    leftRotate(parent);
+	                    fixTree(parent);
+	                } else if (current.isLeftChild() && parent.isRightChild()) {
+	                    rightRotate(parent);
+	                    fixTree(parent);
+	                } else if (current.isLeftChild() && parent.isLeftChild()) {
+	                    parent.setNodeColor(Node.NodeColor.BLACK);
+	                    grandParent.setNodeColor(Node.NodeColor.RED);
+	                    rightRotate(grandParent);
+	                    return;
+	                } else if (current.isRightChild() && parent.isRightChild()) {
+	                    parent.setNodeColor(Node.NodeColor.BLACK);
+	                    grandParent.setNodeColor(Node.NodeColor.RED);
+	                    leftRotate(grandParent);
+	                    return;
+	                }
+	            } else if (aunt.isRed()) {
+	                parent.setNodeColor(Node.NodeColor.BLACK);
+	                aunt.setNodeColor(Node.NodeColor.BLACK);
+	                grandParent.setNodeColor(Node.NodeColor.RED);
+	                fixTree(grandParent);
+	            }
+	        }
+	    }
+
+
+
+
+	    public void preOrderVisit(Visitor v) {
+	        preOrderVisit(root, v);
+	    }
+
+
+	    private static void preOrderVisit(Node node, Visitor visitor) {
+	        if (node == null)
+	            return;
+	        visitor.visit(node);
+	        preOrderVisit(node.getLeftChild(), visitor);
+	        preOrderVisit(node.getRightChild(), visitor);
+	    }
+}
--- src/test/RBTTest.java
+++ src/test/RBTTest.java
@@ -4,23 +4,72 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-import redBlack.ArbolRyN;
+import redBlack.Node;
+import redBlack.RedBlackTree;
+import redBlack.Visitor;
 
 public class RBTTest {
 
-	private ArbolRyN<Integer> rbt;
-	
-	public void stage1() {
-		
-	}
-	
-	@Test
-	public void addNodeTest() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void test() {
+        RedBlackTree redBlackTree = new RedBlackTree();
+        redBlackTree.insert("D");
+        redBlackTree.insert("B");
+        redBlackTree.insert("A");
+        redBlackTree.insert("C");
+        redBlackTree.insert("F");
+        redBlackTree.insert("E");
+        redBlackTree.insert("H");
+        redBlackTree.insert("G");
+        redBlackTree.insert("I");
+        redBlackTree.insert("J");
+        assertEquals("DBACFEHGIJ", makeString(redBlackTree));
+        String str = "Color: 1, Key:D Parent: \n" +
+                "Color: 1, Key:B Parent: D\n" +
+                "Color: 1, Key:A Parent: B\n"+
+                "Color: 1, Key:C Parent: B\n"+
+                "Color: 1, Key:F Parent: D\n"+
+                "Color: 1, Key:E Parent: F\n"+
+                "Color: 0, Key:H Parent: F\n"+
+                "Color: 1, Key:G Parent: H\n"+
+                "Color: 1, Key:I Parent: H\n"+
+                "Color: 0, Key:J Parent: I\n";
+        assertEquals(str, makeStringDetails(redBlackTree));
 
-	@Test
-	public void deleteNodeTest() {
-		fail("Not yet implemented");
-	}
+    }
+
+    public static String makeString(RedBlackTree t) {
+        class MyVisitor implements Visitor {
+            String result = "";
+			@Override
+			public void visit(Node n) {
+				 result = result + n.getKey();
+				
+			}
+        }
+        MyVisitor v = new MyVisitor();
+        t.preOrderVisit(v);
+        return v.result;
+    }
+
+    public static String makeStringDetails(RedBlackTree t) {{
+        class MyVisitor implements Visitor {
+            String result = "";
+            public void visit(Node node) {
+                if(!(node.getKey()).equals("")) {
+                    result = result + "Color: " + node.getNodeColor().ordinal()
+                            + ", Key:" + node.getKey()
+                            + " Parent: ";
+                    if (node.getParent() != null)
+                        result += node.getParent().getKey();
+                    result += "\n";
+                }
+            }
+        }
+        MyVisitor v = new MyVisitor();
+        t.preOrderVisit(v);
+        return v.result;
+    }
+    }
+
 }
